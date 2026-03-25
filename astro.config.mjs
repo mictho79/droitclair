fbf2fc788d16eb37c150c65cd4570df1c568e454
Michael Thomas <mic.thomas@sfr.fr>
2026-03-25 19:49:15 +0100

    maj article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://droitclair.pages.dev',
+  site: 'https://droitclair.fr',
+  integrations: [sitemap()],
   trailingSlash: 'always',
   build: {
     format: 'directory'
